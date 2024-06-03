450eac77ee8cc5b22c87cc0f724929a4ca3c5eb1
Luiz Augusto de Souza Nascimento <164780503+luizaugustodesouza@users.noreply.github.com>
2024-06-03 16:17:28 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-let char = new Mage('Teco');
-let monster = new Sol();
+let charName = window.prompt('Olá herói, antes de embarcar na luta diga-nos seu nome');
+let char = new Mage(charName);
+let monster = new bigMonster();
 let log = new Log(document.querySelector('.log'));
 
 const stage = new Stage(
